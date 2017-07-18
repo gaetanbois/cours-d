b517c7011ca5a4bd29efdddaf9ab0fb50e07d54d
gaetanbois <g.bois17@gmail.com>
2017-07-18 17:01:48 +0200

    boucles

--- popschool-javascript-2/js/boucles.js
+++ popschool-javascript-2/js/boucles.js
@@ -8,7 +8,39 @@ for (var i = 0; i < myArray.length; i++) {
 }
 
 // boucle for en commencant par la fin
-
-for ( var i = myArray.length - 1; i >= 0; i--) {
+for (var i = myArray.length - 1; i >= 0; i--) {
   console.log(myArray[i]);
+}
+
+for (var i in myArray) {
+  console.log(myArray[i]);
+}
+
+for (var value of myArray) {
+  console.log(value);
+  }
+
+
+for (var i = 0; i < myArray.length; i++) {
+  if (myArray[i] > 50) {
+    console.log(myArray[i]);
+  }
+}
+
+for (var i = 0; i < myArray.length; i++) {
+  if (myArray[i] > 0 && myArray[i] < 50) {
+    console.log(myArray[i]);
+  }
+}
+
+for (var value of myArray) {
+  if (Number.isInteger(value)) {
+  console.log(value);
+  }
+}
+
+for (var value of myArray) {
+  if (value - Math.round(value) != 0){
+    console.log(value);
+  }
 }
